21b849011de6e256895865bc5750ec2137ca77ae
tburakonat <tburakonat@gmail.com>
2023-02-25 23:47:15 +0100

    invalidate cache after login and register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,28 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '../theme';
 import { AppProps } from 'next/app';
-import { Provider, createClient } from 'urql';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
 
 const client = createClient({
 	url: 'http://localhost:4000/graphql',
 	fetchOptions: { credentials: 'include' },
+	exchanges: [
+		dedupExchange,
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (_result, _args, cache, _info) => {
+						cache.invalidate('Query', 'me');
+					},
+					register: (_result, _args, cache, _info) => {
+						cache.invalidate('Query', 'me');
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
