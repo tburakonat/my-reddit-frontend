a5e7324096898a82be6bc06b579201da165f4fab
tburakonat <tburakonat@gmail.com>
2023-03-09 21:36:11 +0100

    fix navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 	const [{ data, fetching }] = useMeQuery();
 	const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
 	return (
-		<Flex bg="tomato" p={4} position="sticky" top="0">
+		<Flex bg="tomato" p={4} position="sticky" top="0" zIndex="1">
 			{fetching ? null : data?.me ? (
 				<>
 					<Box mr="auto">Welcome {data.me.username}</Box>
