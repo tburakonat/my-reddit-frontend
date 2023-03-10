870edb3d0b3a6e85f9adc56f9938df73a7f58949
tburakonat <tburakonat@gmail.com>
2023-03-10 20:02:51 +0100

    update cache and forward cookie to graphql api

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,7 +1,9 @@
-import { dedupExchange, Exchange, fetchExchange, stringifyVariables } from 'urql';
+import { dedupExchange, Exchange, fetchExchange, gql, stringifyVariables } from 'urql';
 import { cacheExchange, Resolver } from '@urql/exchange-graphcache';
 import { pipe, tap } from 'wonka';
 import Router from 'next/router';
+import { VoteMutationVariables } from '../generated/graphql';
+import { isServer } from './isServer';
 
 const errorExchange: Exchange =
 	({ forward }) =>
@@ -55,42 +57,118 @@ const simplePagination = (): Resolver<any, any, any> => {
 	};
 };
 
-export const createUrqlClient = (ssrExchange: any) => ({
-	url: 'http://localhost:4000/graphql',
-	fetchOptions: { credentials: 'include' as const },
-	exchanges: [
-		dedupExchange,
-		cacheExchange({
-			keys: {
-				PaginatedPosts: () => null,
-			},
-			resolvers: {
-				Query: {
-					posts: simplePagination(),
+export const createUrqlClient = (ssrExchange: any, ctx: any) => {
+	let cookie = '';
+	if (isServer()) {
+		cookie = ctx.req.headers.cookie;
+	}
+
+	return {
+		url: 'http://localhost:4000/graphql',
+		fetchOptions: {
+			credentials: 'include' as const,
+			headers: cookie ? { cookie } : undefined,
+		},
+		exchanges: [
+			dedupExchange,
+			cacheExchange({
+				keys: {
+					PaginatedPosts: () => null,
 				},
-			},
-			updates: {
-				Mutation: {
-					login: (_result, _args, cache, _info) => {
-						cache.invalidate('Query', 'me');
-					},
-					register: (_result, _args, cache, _info) => {
-						cache.invalidate('Query', 'me');
-					},
-					logout: (_result, _args, cache, _info) => {
-						cache.invalidate('Query', 'me');
-					},
-					createPost: (_result, _args, cache, _info) => {
-						cache.invalidate('Query', 'posts', {
-							limit: 15,
-							cursor: null,
-						});
+				resolvers: {
+					Query: {
+						posts: simplePagination(),
 					},
 				},
-			},
-		}),
-		errorExchange,
-		ssrExchange,
-		fetchExchange,
-	],
-});
+				updates: {
+					Mutation: {
+						vote: (_result, args, cache, _info) => {
+							const { postId, value } = args as VoteMutationVariables;
+
+							const data = cache.readFragment(
+								gql`
+									fragment _ on PostType {
+										id
+										points
+										voteStatus
+									}
+								`,
+								{ id: postId }
+							);
+
+							if (data) {
+								// the user has voted on the post before
+								// and they are again voting the same way
+								// we remove their vote
+								if (data.voteStatus === value) {
+									cache.writeFragment(
+										gql`
+											fragment _ on PostType {
+												id
+												voteStatus
+												points
+											}
+										`,
+										{ id: postId, voteStatus: null, points: data.points - value }
+									);
+								}
+
+								// the user has not voted on the post before
+								// we adjust the points by 1
+								if (data.voteStatus === null) {
+									cache.writeFragment(
+										gql`
+											fragment _ on PostType {
+												id
+												voteStatus
+												points
+											}
+										`,
+										{ id: postId, voteStatus: value, points: data.points + value }
+									);
+								}
+
+								// the user has voted on the post before
+								// and they are changing their vote
+								// we adjust the points by 2
+								if (data.voteStatus !== null && data.voteStatus !== value) {
+									cache.writeFragment(
+										gql`
+											fragment _ on PostType {
+												id
+												voteStatus
+												points
+											}
+										`,
+										{ id: postId, voteStatus: value, points: data.points + 2 * value }
+									);
+								}
+							}
+						},
+						login: (_result, _args, cache, _info) => {
+							cache.invalidate('Query', 'posts', { limit: 15 });
+							cache.invalidate('Query', 'me');
+						},
+						register: (_result, _args, cache, _info) => {
+							cache.invalidate('Query', 'posts', { limit: 15 });
+							cache.invalidate('Query', 'me');
+						},
+						logout: (_result, _args, cache, _info) => {
+							cache.invalidate('Query', 'posts', { limit: 15 });
+							cache.invalidate('Query', 'me');
+						},
+						createPost: (_result, _args, cache, _info) => {
+							cache.invalidate('Query', 'posts', {
+								limit: 15,
+								cursor: null,
+							});
+						},
+					},
+				},
+			}),
+			errorExchange,
+			ssrExchange,
+			fetchExchange,
+		],
+	};
+};
